613b6db1cd895f20e6deab4715ede4738fd5b3cd
PhilemonCleland <philemoncleland@gmail.com>
2019-07-18 12:07:12 +0000

    Todo_list

--- todo_server.py
+++ todo_server.py
@@ -1,7 +1,8 @@
 
 from flask import *
-import os
+
 import json
+import os
  
 app = Flask(__name__)
 
@@ -32,4 +33,5 @@ if __name__ == '__main__':
     port = os.environ.get('PORT',5000)
     app.run(debug=True, host='0.0.0.0', port = port)
     
+
 
