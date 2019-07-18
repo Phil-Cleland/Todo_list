282fb0ef14b7008319563612b36e0eb2b04546c6
PhilemonCleland <philemoncleland@gmail.com>
2019-07-18 11:57:14 +0000

    deployed files

--- todo_server.py
+++ todo_server.py
@@ -1,5 +1,7 @@
 
 from flask import *
+import os
+import json
  
 app = Flask(__name__)
 
@@ -27,6 +29,7 @@ def foo(name):
     return render_template('index.html', to=name)
  
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    port = os.environ.get('PORT',5000)
+    app.run(debug=True, host='0.0.0.0', port = port)
     
 
